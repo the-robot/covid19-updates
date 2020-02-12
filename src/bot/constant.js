5c0709ab7941a0554a2b2fdf7a02fe68ee83cbda
the-robot <the-robot@users.noreply.github.com>
2020-02-12 13:23:43 +0800

    fixed wrong channel id

--- src/bot/constant.js
+++ src/bot/constant.js
@@ -1,5 +1,5 @@
 const ABOUT_VIRUS = 'https://www.who.int/health-topics/coronavirus';
 const GITHUB_URL = 'https://github.com/the-robot/corona-bot';
-const TELEGRAM_CHANNEL = 'novelcov-19';
+const TELEGRAM_CHANNEL = 'novelcov19';
 
 export { ABOUT_VIRUS, GITHUB_URL, TELEGRAM_CHANNEL };
